a2b5f6aefd308c7af0aa4e5d9e5d2292f8c37ddd
histerix <3678611+histeriks@users.noreply.github.com>
2023-01-07 17:58:24 +0100

    Update zfs.sh

--- zfs.sh
+++ zfs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 echo -e "\e[93mYour ZFS pool name?\e[0m"; read ZPOOL
+
 zfs set compression=on $ZPOOL
 zfs set compression=lz4 $ZPOOL
 zfs set primarycache=all $ZPOOL
@@ -8,4 +10,12 @@ zfs set relatime=off $ZPOOL
 zfs set checksum=on $ZPOOL
 zfs set dedup=off $ZPOOL
 zfs set xattr=sa $ZPOOL
+
+# zfs create -V 8G -b 4096 -o logbias=throughput -o sync=always -o primarycache=metadata -o com.sun:auto-snapshot=false $ZPOOL/swap
+# mkswap -f /dev/zvol/$ZPOOL/swap
+# swapon /dev/zvol/$ZPOOL/swap
+# cat "/dev/zvol/$ZPOOL/swap none swap discard 0 0" >> /etc/fstab
+# echo "Partition optimized for MySQL/PostgreSQL"
+# zfs create -o recordsize=8K -o primarycache=metadata -o mountpoint=/rdbms -o logbias=throughput $ZPOOL/rdbms
+
 exit 0;
